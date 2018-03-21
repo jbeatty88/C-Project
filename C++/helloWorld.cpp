ac0fd10d5dd6e3c91c2c0a8f204ca04ba1400757
Josh Beatty <joshuabeatty88@gmail.com>
2018-03-21 09:42:33 -0600

    Hello World in C++

---
+++ C++/helloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+  string helloWorld = "Hello World";
+
+  cout << helloWorld << endl;
+
+  return 0;
+}
