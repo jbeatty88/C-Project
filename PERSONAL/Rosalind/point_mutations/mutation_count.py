1b9906ac3e79bb5aa1df6a1e13e74f9b452e8f84
jbeatty88 <joshuabeatty88@gmail.com>
2018-08-13 22:28:47 -0600

    point mutations

---
+++ PERSONAL/Rosalind/point_mutations/mutation_count.py
@@ -0,0 +1,14 @@
+import numpy as np
+import sys
+
+input_file = sys.argv[1]
+seq_arr = np.array([])
+
+with open(input_file, 'r') as dna_seq:
+    sequences = dna_seq.readlines()
+    
+for line in sequences:
+    seq_arr = np.append(seq_arr, line)
+
+print(seq_arr[1:] == seq_arr[:-1])
+
